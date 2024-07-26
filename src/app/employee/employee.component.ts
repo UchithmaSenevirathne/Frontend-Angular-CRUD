63fa297ea1e53dd7819a391779e182e8663f5255
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-26 16:45:02 +0530

    create employee obj

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -4,15 +4,19 @@ import { Employee } from '../employee.model';
 @Component({
   selector: 'app-employee',
   templateUrl: './employee.component.html',
-  styleUrl: './employee.component.scss'
+  styleUrl: './employee.component.scss',
 })
 export class EmployeeComponent implements OnInit {
-  employee: Employee;
-constructor(){
+  employee: Employee = {
+    employeeId: 0,
+    employeeName: '',
+    employeeContactNumber: '',
+    employeeAddress: '',
+    employeeGender: '',
+    employeeDepartment: '',
+    employeeSkills: '',
+  };
+  constructor() {}
 
-}
-
-ngOnInit(): void {
-  
-}
+  ngOnInit(): void {}
 }
