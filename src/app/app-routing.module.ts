d22dcca967310a02dbb08a5099ebde7968c2d6e7
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-26 21:19:17 +0530

    add resolver in app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeResolver } from './employee.resolver';
 
 const routes: Routes = [
   {path: 'header', component: HeaderComponent},
-  {path: 'employee', component: EmployeeComponent},
+  {path: 'employee', component: EmployeeComponent, resolve: {employee: EmployeeResolver}},
   {path: 'employee-list', component: EmployeeListComponent},
   {path: '', component: HomeComponent}
 ];
