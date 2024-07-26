3c7a201761c375ffff6de156cd25ecd7b59aa38e
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-26 21:08:44 +0530

    some updates employee resolver

---
+++ src/app/employee.resolver.ts
@@ -0,0 +1,24 @@
+import {
+  ActivatedRouteSnapshot,
+  ResolveFn,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { EmployeeService } from './employee.service';
+import { inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from './employee.model';
+
+export const EmployeeResolver: ResolveFn<any> = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot,
+  employeeService: EmployeeService = inject(EmployeeService)
+): Observable<Employee> => {
+    const employeeId = route.paramMap.get("employeeId");
+
+    if(employeeId){
+        //make api call and get data for given employee id
+        return this.employeeService.getEmployees
+    }else{
+        //create and return empty employee details
+    }
+};
