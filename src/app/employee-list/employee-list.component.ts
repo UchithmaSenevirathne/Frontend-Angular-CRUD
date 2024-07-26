85fe6c3ef7637140012174f0320e7e45d9c369d8
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-26 19:50:15 +0530

    get employee list to console

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee.model';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
-  styleUrl: './employee-list.component.scss'
+  styleUrl: './employee-list.component.scss',
 })
 export class EmployeeListComponent implements OnInit {
-
-  constructor(){}
-
-  ngOnInit(): void {
-    
+  constructor(private employeeService: EmployeeService) {
+    this.getEmployeeList();
   }
 
+  ngOnInit(): void {}
+
+  getEmployeeList(): void {
+    this.employeeService.getEmployees().subscribe({
+      next: (res: Employee[]) => {
+        console.log(res);
+      },
+      error: (err: HttpErrorResponse) => {
+        console.log(err);
+      },
+    });
+  }
 }
