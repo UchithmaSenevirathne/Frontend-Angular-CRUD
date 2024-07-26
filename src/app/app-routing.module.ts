19f0c4323d637c153a2f86ba293a7fdc0f410132
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-26 09:21:44 +0530

    give path employee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 const routes: Routes = [
   {path: 'header', component: HeaderComponent},
+  {path: 'employee', component: EmployeeComponent},
   {path: '', component: HomeComponent}
 ];
 
