2f6d993a0d1ec7f21660b8c7a8c17b9f86063469
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-26 08:40:14 +0530

    add path to home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: 'header', component: HeaderComponent}
+  {path: 'header', component: HeaderComponent},
+  {path: '', component: HomeComponent}
 ];
 
 @NgModule({
